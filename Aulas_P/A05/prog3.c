2b620a5bc4f5e4ea6344a9f8e85501c296487327
Ze Paiva <jose.paiva@ua.pt>
2018-03-15 09:25:16 +0000

    found error, it was a semi-colon

--- Aulas_P/A05/prog3.c
+++ Aulas_P/A05/prog3.c
@@ -11,7 +11,7 @@ int main()
   AD1CON1bits.SSRC = 7;
   AD1CON1bits.CLRASAM = 1;
   AD1CHSbits.CH0SA = 4;
-  AD1CON2bits.SMPI = 15;
+  AD1CON2bits.SMPI = 5;
   AD1CON3bits.SAMC = 16;
   AD1CON1bits.ON = 1;
   
@@ -25,21 +25,21 @@ int main()
     printStr("\n\nBuffers 0-F:\n");
     bp = (int *)(&ADC1BUF0);
     int i;
+    
+   /* 
     for (i = 0; i < 16; i++)
     {
       printInt(bp[i*4], 16 | 3 << 16);
       printStr("\n");
     }
-    
-    // solution with pointers only isn't working
-    // have to check with teacher
-    /*
-    for(; bp <= (int *)(&ADC1BUFF); bp+=4);
+    */
+   
+    for(; bp <= (int *)(&ADC1BUFF); bp+=4)
     {
       printInt(*bp, 16 | 3 << 16);
       printStr("\n");
     }
-    */
+    
 
     LATBbits.LATB6 = 0;
     aux = ADC1BUF0;
